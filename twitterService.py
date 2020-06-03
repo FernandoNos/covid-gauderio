b7d327826750b2659b44d38a6622350f42cd9ec7
Fernando <fernando_nos@sicredi.com.br>
2020-06-02 21:56:48 -0300

    testing

--- twitterService.py
+++ twitterService.py
@@ -1,5 +1,9 @@
 import tweepy
 
+consumer_key = 'RgAe9IG9Wimi6T9lsY7BARGQG'
+consumer_secret = 'uxYLXocChG0zJfu5BWJWDq32yFhN9cl7qWgy8SGAhsLpk5jFNz'
+access_token = '3007843847-5oDfaubqPTMYoIlSpF4m9vBFZyg0Cym3aGAV1Jv'
+access_token_secret = 'aSQDOXMrIVkla5y4lmGzy1Zw3q9241opXedRRTiMUDT56'
 
 def sendTweet(message):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
