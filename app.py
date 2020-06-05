ec33fb8d2608f286587b3ea700e970bb37c7e758
Fernando <fernando_nos@sicredi.com.br>
2020-06-04 22:05:03 -0300

    env vars

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ from twitterService import sendTweet
 from covidStatusService import getRSCovidStatuses
 import schedule
 import time
+from datetime import datetime
 
 def sendCovidUpdates():
-    statuses = getRSCovidStatuses()
 
+    print('Starting to send out updates!')
+    statuses = getRSCovidStatuses()
+    print('Statuses retrieved!')
     for status in statuses:
         title = status['titulo']
         flag = status['bandeira']
         description = status['bandeira_descricao']
+        print('  Sending update for '+title)
         tweet = title + ' está na bandeira '+flag+'. \nDescrição: '+description
 
         sendTweet(tweet)
+        break
+    
+
+    # Current date time in local system
+    print(datetime.now())
+    print('All updates sent!')
 schedule.every(1).minutes.do(sendCovidUpdates)
 
 while True:
--- twitterService.py
+++ twitterService.py
@@ -1,5 +1,12 @@
 import tweepy
+import os
 
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+print('Variables Set!')
 
 def sendTweet(message):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
