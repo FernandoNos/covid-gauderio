193bd147216766963a68dd98d5f7cf2836f4b5b2
Fernando <fernando_nos@sicredi.com.br>
2020-06-04 21:55:26 -0300

    including setup.pt

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='CoviTche',
+      version='1.0',
+      description='Covid updates',
+      author='Fernando Nos',
+      author_email='nosfernandos@gmail.com',
+      packages=setuptools.find_packages(),
+      install_requires=["tweepy","requests"]
+     )
