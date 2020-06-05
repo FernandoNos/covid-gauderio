b8cc6e5056cc172fd801c77e56442f566823bd6e
Fernando <fernando_nos@sicredi.com.br>
2020-06-04 23:35:15 -0300

    testing

--- twitterService.py
+++ twitterService.py
@@ -5,6 +5,7 @@ consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 access_token = os.getenv('ACCESS_TOKEN') 
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+print(os.getenv('TEST'))
 
 print('Variables Set!')
 
